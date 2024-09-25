cd588d8dd3bc89ec248daa8e86f81bf9045da92c
Neo Prudence <neoboledi21@gmail.com>
2024-09-25 17:54:01 +0200

    my quiz

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let conf = confirm("Ready to answer a few questions?");
+
+if(conf === true){
+
+  let name =  prompt("What is your name?");
+  
+  if(name === null ){
+
+  }
+  else{
+    let age = prompt("How old are you?");
+
+    if(age===null){
+
+    }else{
+        let place= prompt("Where do you stay?");
+
+        if(place === null){
+
+        }else{
+            let color = prompt("What is your favourite color?");
+        }
+    }
+  }
+}else{
+
+}
